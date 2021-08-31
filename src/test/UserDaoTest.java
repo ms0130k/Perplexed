ab28d4bcb6bee0437cf041525dca07b8946a52f2
Teddy-PC2 <Teddy-PC2@DESKTOP-DHTV61C>
2021-09-01 07:03:15 +0900

    ApplicationContext를 통한 싱글톤 확인

--- src/test/UserDaoTest.java
+++ src/test/UserDaoTest.java
@@ -12,15 +12,37 @@ import springbook.user.domain.User;
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//		UserDao dao = context.getBean("userDao", UserDao.class);
+//		dao = (UserDao) context.getBean("userDao");
+//		dao.deleteAll();
+//		User user = new User();
+//		user.setId("아이디");
+//		user.setName("이름");
+//		user.setPassword("비밀번호");
+//		dao.add(user);
+//		System.out.println(dao.get("아이디"));
+		
+		DaoFactory factory = new DaoFactory();
+		UserDao dao1 = factory.userDao();
+		UserDao dao2 = factory.userDao();
+		System.out.println(dao1 == dao2);
+		System.out.println(dao1.equals(dao2));
+		
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao dao = context.getBean("userDao", UserDao.class);
-		dao = (UserDao) context.getBean("userDao");
-		dao.deleteAll();
-		User user = new User();
-		user.setId("아이디");
-		user.setName("이름");
-		user.setPassword("비밀번호");
-		dao.add(user);
-		System.out.println(dao.get("아이디"));
+		dao1 = context.getBean("userDao", UserDao.class);
+		dao2 = context.getBean("userDao", UserDao.class);
+		System.out.println(dao1 == dao2);
+		System.out.println(dao1.equals(dao2));
+		
+		dao1 = (UserDao) context.getBean("userDao");
+		dao2 = (UserDao) context.getBean("userDao");
+		System.out.println(dao1 == dao2);
+		System.out.println(dao1.equals(dao2));
+		System.out.println("====");
+		System.out.println(new String("Str") == new String("Str"));
+		System.out.println(new String("Str").equals(new String("Str")));
+		
+		
 	}
 }
