fb0367832bf2e51b9e5e5eb395ecb1636e6930ea
Teddy-PC2 <Teddy-PC2@DESKTOP-DHTV61C>
2021-09-08 12:57:53 +0900

    DirtiesContext / SingleConnectionDataSource 사용

--- src/springbook/user/domain/JDBCInfo.java
+++ src/springbook/user/domain/JDBCInfo.java
@@ -2,8 +2,6 @@ package springbook.user.domain;
 
 public enum JDBCInfo {
 
-//	Class.forName("com.mysql.cj.jdbc.Driver");
-//	return DriverManager.getConnection("jdbc:mysql://34.64.107.165/shock_spring_db?useSSL=true", "shock_user", "1205cnlcla@@");
 	DRIVER_CLASS("com.mysql.cj.jdbc.Driver"),
 	URL("jdbc:mysql://34.64.107.165/shock_spring_db?useSSL=true"),
 	USERNAME("shock_user"),
--- src/test/UserDaoTest.java
+++ src/test/UserDaoTest.java
@@ -5,20 +5,25 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.dao.UserDao;
+import springbook.user.domain.JDBCInfo;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/applicationContext.xml")
+@DirtiesContext
 public class UserDaoTest {
 	@Autowired
 	private UserDao dao;
@@ -31,6 +36,9 @@ public class UserDaoTest {
 		user1 = new User("shock1", "동현1", "비번1");
 		user2 = new User("shock2", "동현2", "비번2");
 		user3 = new User("shock3", "동현3", "비번3");
+		
+		SingleConnectionDataSource dataSource = new SingleConnectionDataSource(JDBCInfo.URL.getValue(), JDBCInfo.USERNAME.getValue(), JDBCInfo.PASSWORD.getValue(), true);
+		dao.setDataSource(dataSource);
 	}
 	
 	@Test
