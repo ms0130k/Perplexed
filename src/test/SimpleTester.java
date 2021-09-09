e71194c2e7c5c7ef17cd538d5ad59e8c00d41bf0
Teddy-PC2 <Teddy-PC2@DESKTOP-DHTV61C>
2021-09-09 17:00:35 +0900

    JUnitTest 객체를 JUnit으로 테스트

--- src/test/SimpleTester.java
+++ src/test/SimpleTester.java
@@ -1,11 +1,24 @@
 package test;
 
-import springbook.user.domain.JDBCInfo;
-
 public class SimpleTester {
 	
+	public static int count;
+	
 	public static void main(String[] args) {
-		System.out.println(JDBCInfo.URL.valueOf("URL"));
-		String url = "";
+		SimpleTester st1 = new SimpleTester();
+		System.out.println(st1.count);
+		st1.count++;
+		SimpleTester st2 = new SimpleTester();
+		System.out.println(st2.count);
+		st2.count++;
+		SimpleTester st3 = new SimpleTester();
+		System.out.println(st3.count);
+		st3.count++;
+		SimpleTester st4 = new SimpleTester();
+		System.out.println(st4.count);
+		st4.count++;
+		
+		System.out.println(st1 == st2);
+		System.out.println(st1.equals(st2));
 	}
 }
