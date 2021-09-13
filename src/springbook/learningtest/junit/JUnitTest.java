c453e44d2a35e8065b63e74c593b06fe6e0a649f
Teddy-PC2 <Teddy-PC2@DESKTOP-DHTV61C>
2021-09-13 17:02:57 +0900

    같은 context를 사용하는지 junit 테스트

--- src/springbook/learningtest/junit/JUnitTest.java
+++ src/springbook/learningtest/junit/JUnitTest.java
@@ -2,28 +2,51 @@ package springbook.learningtest.junit;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/springbook/learningtest/junit/junit.xml")
 public class JUnitTest {
-	static JUnitTest testObject;
+	@Autowired
+	ApplicationContext context;
+	
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+	static ApplicationContext contextObject = null;
 	
 	@Test
 	public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println(this);
+		
+		assertThat(contextObject == null || contextObject == context, is(true));
+		contextObject = context;
 	}
 	@Test
 	public void test2() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println(this);
+		assertThat(contextObject == null || contextObject == context, is(true));
+		contextObject = context;
 	}
 	@Test
 	public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObjects, not(hasItem(this)));
+		testObjects.add(this);
+		System.out.println(this);
+		assertThat(contextObject == null || contextObject == context, is(true));
+		contextObject = context;
 	}
 }
